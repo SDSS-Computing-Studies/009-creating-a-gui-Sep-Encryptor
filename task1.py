c5f077aac29cb69bba1bb2bec734e4f5b4b19d45
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-04-15 19:47:23 -0700

    done

--- task1.py
+++ task1.py
@@ -1,21 +1,20 @@
 #!python3
 
-import tkinter as tk 
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
 window = tk.Tk()
 
-l1 = tk.Label(window,text="x")
-b1 = tk.Button(window,text="=")
-e1 = tk.Entry(window, borderwidth=3)
-e2 = tk.Entry(window, borderwidth=3)
-e3 = tk.Entry(window, borderwidth=3)
+l1 = tk.Label(window, text="x")
+b1 = tk.Button(window, text="=")
+e1 = tk.Enrty(window, borderwidth = 3)
+e3 = tk.Enrty(window, borderwidth = 3)
 
 e1.grid (row = 1, column = 1)
 l1.grid (row = 1, column = 2)
 e2.grid (row = 1, column = 3)
-b1.grid (row = 1, column =4)
+b1.grid (row = 1, column = 4)
 e3.grid (row = 1, column = 5)
-
+ 
 window.mainloop()

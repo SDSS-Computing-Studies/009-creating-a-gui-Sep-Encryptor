ff9d802ca6052e81cec9915ec54a34095319c719
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-04-15 18:53:40 -0700

    done

--- task1.py
+++ task1.py
@@ -1,24 +1,24 @@
 #!python3
 
-import tkinter as tk
+import tkinter as tk 
 from tkinter import *
-from tkinter import Tk
+from tkinter import ttk
 
 window = tk.Tk()
-window.title("Calculator")
+window.title("Calculater")
 window.geometry("500x20")
 
-e1 = tk.Entry(window,width = 30)
-l1 = tk.Label(window,text = "X")
-e2 = tk.Entry(window,width = 30)
-l2 = tk.Label(window,text = "=")
-e3 = tk.Enrty(window,width = 20)
+entry1 = tk.Entry(window,width = 30)
+label1 = tk.Label(window,text = "X")
+entry2 = tk.Entry(window,width = 30)
+label2 = tk.Label(window,text = "=")
+entry3 = tk.Entry(window,width = 20)
 
-e1.grid(row = 1,column = 1)
-l1.grid(row = 1,column = 2)
-e2.grid(row = 1,column = 3)
-l2.grid(row = 1,column = 4)
-e3.grid(row = 1,column = 5)
+entry1.grid(row = 1,column = 1)
+label1.grid(row = 1,column = 2)
+entry2.grid(row = 1,column = 3)
+label2.grid(row = 1,column = 4)
+entry3.grid(row = 1,column = 5)
 
 window.mainloop()
 

f0cd16d40bb938abcbd3bd4e40d0d46d9430e91a
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-04-15 19:09:06 -0700

    done

--- task2.py
+++ task2.py
@@ -1,39 +1,48 @@
+#!python3
+
 import tkinter as tk 
 from tkinter import *
 from tkinter import ttk
 
 window = tk.Tk()
-window.title("T-Town Veterinary Clinic Database")
+window.title("Window")
 window.geometry("600x200")
 
-label1 = tk.Label(window,text="Client Database")
-label2 = tk.Label(window,text="Name")
-label3 = tk.Label(window,text="Type")
-label4 = tk.Label(window,text="Breed")
-label5 = tk.Label(window,text="Owner")
-label6 = tk.Label(window,text="Birthdate")
-
-
 dogphoto = PhotoImage(file="dog.png")
-label8 = tk.Label(window, image=dogphoto)
+label1 = tk.Label(window, image = dogphoto,borderwidth = 3, relief = SUNKEN)
+button1 = tk.Button(window,text = "Search by Name")
+entry1 = tk.Entry(window,width = 20)
+label2 = tk.Label(window,text = "Client Database")
+label3 = tk.Label(window,text = "Name")
+label4 = tk.Label(window,text = "Type")
+label5 = tk.Label(window,text = "Breed")
+label6 = tk.Label(window,text = "Owner")
+label7 = tk.Label(window,text = "Birthdate")
+entry2 = tk.Entry(window,width = 20)
+entry3 = tk.Entry(window,width = 20)
+entry4 = tk.Entry(window,width = 20)
+entry5 = tk.Entry(window,width = 20)
+entry6 = tk.Entry(window,width = 20)
+button2 = tk.Button(window,text = "<Previous")
+button3 = tk.Button(window,text = "Save Entry")
+button4 = tk.Button(window,text = "Next>")
 
-button1 = tk.Button(window,text="<Previous")
-button2 = tk.Button(window,text="Save Entry")
-button3 = tk.Button(window,text="Next>")
+label1.grid(row = 1, column = 1, rowspan = 2)
+button1.grid(row = 1,column = 4)
+entry1.grid(row = 1,column = 5)
+label2.grid(row = 2,column = 3)
+label3.grid(row = 3,column = 1)
+label4.grid(row = 3,column = 2)
+label5.grid(row = 3,column = 3)
+label6.grid(row = 3,column = 4)
+label7.grid(row = 3,column = 5)
+entry2.grid(row = 4,column = 1)
+entry3.grid(row = 4,column = 2)
+entry4.grid(row = 4,column = 3)
+entry5.grid(row = 4,column = 4)
+entry6.grid(row = 4,column = 5)
+button2.grid(row = 5,column = 1)
+button3.grid(row = 5,column = 3)
+button4.grid(row = 5,column = 5)
 
-button7 = tk.Button(window,text="search by name",width=15)
-
-entry1 = tk.Entry(window,text="", width=15)
-entry2 = tk.Entry(window,text="", width=15)
-entry3 = tk.Entry(window,text="", width=15)
-entry4 = tk.Entry(window,text="", width=15)
-entry5 = tk.Entry(window,text="", width=15)
-entry6 = tk.Entry(window,text="", width=25)
-
-
-button1.place(x=10,y=175)
-button2.place(x=270,y=175)
-button3.place(x=550,y=175)
-
-entry1.place(x=10,y=150)
-entry2.place(x=130,y=150)
+window.mainloop()

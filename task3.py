9de23fa36a7735c1422489692d78007d14153079
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-04-19 00:45:58 -0700

    done

--- task3.py
+++ task3.py
@@ -12,5 +12,8 @@ poc=tk.Label(window, image=dogphoto)
 poc=tk.Label(window, text='Pochacco!')
 label1=tk.Label(window,text='A cuddly little puppy! This is from the same /n creaters who brought you Keropi and Kero', bg = "#87ceeb")
 
+photo.place(x=60,y=0)
+poc.place(x=130,y=40)
+label1.place(x=0,y=100)
 
 window.mainloop()

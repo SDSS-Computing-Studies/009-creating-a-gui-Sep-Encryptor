f59b7a009531b19af838eb8085fab84de203825d
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-04-19 00:55:10 -0700

    done

--- task1.py
+++ task1.py
@@ -5,18 +5,20 @@ from tkinter import *
 from tkinter import ttk
 
 window = tk.Tk()
+window.title("Calculater")
+window.geometry("500x20")
 
-label1 = tk.Label(window,text="x")
-button1 = tk.Button(window,text="=")
-entry1 = tk.Entry(window,borderwidth=3)
-enrty2 = tk.Entry(window,borederwidth=3)
-entry3 = tk.Entry(window,borederwidth=3)
+entry1 = tk.Entry(window,width = 30)
+label1 = tk.Label(window,text = "X")
+entry2 = tk.Entry(window,width = 30)
+label2 = tk.Label(window,text = "=")
+entry3 = tk.Entry(window,width = 20)
 
-entry1.grid (row = 1, column = 1)
-label1.grid (row = 1, column = 2)
-enrty2.grid (row = 1, column = 3)
-button1.grid (row = 1, column = 4)
-entry3.grid (row = 1, column = 5)
+entry1.grid(row = 1,column = 1)
+label1.grid(row = 1,column = 2)
+entry2.grid(row = 1,column = 3)
+label2.grid(row = 1,column = 4)
+entry3.grid(row = 1,column = 5)
 
 window.mainloop()
 
